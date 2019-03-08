ec8b9ac21af8ec124eb5495507854fca38c9dbc1
Adrian Nagy <drnnagy@gmail.com>
2019-03-08 22:06:08 +0100

    set logging level from env resolves #4

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 import falcon
 import logging, sys
+import os
 from signer.staking import StopStaking, StartStaking
 from signer.sign import KeysResource
 from signer.register import Register
 from signer.authorized import Authorized
 from signer.log_requests import RequestLogger
 
-logging.basicConfig(stream=sys.stdout, format='%(asctime)s %(message)s', level=logging.DEBUG)
+# set logging level from ENV variable
+log_level = logging.INFO
+try:
+    if os.environ['LOGLEVEL'] == "DEBUG":
+        log_level = logging.DEBUG
+except KeyError:
+    pass
+
+logging.basicConfig(stream=sys.stdout, format='%(asctime)s %(message)s', level=log_level)
 
 # create application instance
 api = application = falcon.API(middleware=RequestLogger())
